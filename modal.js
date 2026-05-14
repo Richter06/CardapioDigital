693b3ee48f10c1a66938a3ef1d15490795159e42
Richter06 <richardrodriguis09@gmail.com>
2026-05-14 20:39:30 -0300

    Adição do card em destaque quando cliente clica nos produtos em destaque

---
+++ modal.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll('.highlight-card');
+
+cards.forEach(card => {
+
+    card.addEventListener('click', () => {
+
+        cards.forEach(otherCard => {
+
+            if (otherCard !== card) {
+                otherCard.classList.remove('active');
+            }
+
+        });
+
+        card.classList.toggle('active');
+
+    });
+
+});
